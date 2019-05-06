b90ee30752ed70499dcfba06dcca71523f8d495a
lpile <logan.pile@gmail.com>
2019-05-06 13:40:15 -0600

    Add books index feature spec
    Co-authored-by: CosmicSpagetti <billyurrutia@gmail.com>
    Co-authored-by: lpile <logan.pile@gmail.com>

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "books index page", type: :feature do
+  before :each do
+    @book_1 = Book.create!(title: "Title 1", pages: 123, authors: "Author 1", published: 1999)
+    @book_2 = Book.create!(title: "Title 2", pages: 221, authors: "Author 2", published: 2010)
+  end
+
+  describe "As a visitor" do
+    describe "when I visit a book index page" do
+      it "I see all book titles in the database" do
+        visit '/books'
+
+        expect(page).to have_content(@book_1.title)
+        expect(page).to have_content(@book_2.title)
+      end
+    end
+  end
+end
