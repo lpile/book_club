8d19638ae374cbee8b076555954484a51517f6ff
lpile <logan.pile@gmail.com>
2019-05-06 14:39:33 -0600

    Add image column to books table
    Co-authored-by: CosmicSpagetti <billyurrutia@gmail.com>
    Co-authored-by: lpile <logan.pile@gmail.com>

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe "books index page", type: :feature do
   before :each do
-    @book_1 = Book.create!(title: "Title 1", pages: 123, authors: "Author 1", published: 1999)
-    @book_2 = Book.create!(title: "Title 2", pages: 221, authors: "Author 2", published: 2010)
+    @book_1 = Book.create!(title: "Title 1", pages: 123, authors: "Author 1", published: 1999, image: "image_1.jpg")
+    @book_2 = Book.create!(title: "Title 2", pages: 221, authors: "Author 2", published: 2010, image: "image_2.jpg")
   end
 
   describe "As a visitor" do
