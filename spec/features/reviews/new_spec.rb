418b9a96635d97688228c8da9464ff754554f441
lpile <logan.pile@gmail.com>
2019-05-11 15:01:15 -0600

    Add edge cases spec

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -22,11 +22,48 @@ RSpec.describe "Reviews New Page", type: :feature do
 
           new_review = Review.last
 
-          expect(current_path).to eq(review_path(new_review))
-          expect(page).to have_content(title)
-          expect(page).to have_content(user)
-          expect(page).to have_content(rating)
-          expect(page).to have_content(comment)
+          expect(current_path).to eq(book_path(new_book))
+          expect(page).to have_content(new_review.title)
+          expect(page).to have_content(new_review.user)
+          expect(page).to have_content(new_review.rating)
+          expect(page).to have_content(new_review.comment)
+        end
+      end
+      context "and correct information is filled" do
+        it "so reviews title and user are title case" do
+          new_book = Book.create!(title: "Title 1", pages: 123, published: 1999, image: "http://clipart-library.com/images/6cr5yaAqi.png")
+
+          visit new_book_review_path(new_book)
+
+          fill_in 'Title', with: "first review"
+          fill_in 'User', with: "logan"
+          fill_in 'Rating', with: 3
+          fill_in 'Comment', with: 'test comment 1'
+
+          click_on 'Create Review'
+
+          review_1 = Review.last
+
+          expect(review_1.title).to eq("First Review")
+          expect(review_1.user).to eq("Logan")
+        end
+
+        it "so user cannot have two reviews on same book" do
+          new_book = Book.create!(title: "Title 1", pages: 123, published: 1999, image: "http://clipart-library.com/images/6cr5yaAqi.png")
+          review_1 = new_book.reviews.create(title: "First Review", user: "Logan", rating: 3, comment: "Test comment 2")
+
+          visit new_book_review_path(new_book)
+
+          fill_in 'Title', with: "second review"
+          fill_in 'User', with: "logan"
+          fill_in 'Rating', with: 3
+          fill_in 'Comment', with: 'test comment 2'
+
+          click_on 'Create Review'
+
+          test_review = Review.last
+
+          expect(review_1).to eq(test_review)
         end
       end
     end
