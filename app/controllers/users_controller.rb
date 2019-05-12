6fad87ec5c955381b02a958b841f3a7d10d10496
lpile <logan.pile@gmail.com>
2019-05-11 22:22:01 -0600

    Add users#destroy action and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < ApplicationController
     @user = params[:id]
     @user_reviews = Review.all.select {|review| review.user == params[:id]}
   end
+
+  def destroy
+    @review = Review.find(params[:rev_id])
+    @review.destroy
+
+    redirect_to user_path(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   resources :reviews, except: :index
 
-  resources :users, only: :show
+  resources :users, only: [:show, :destroy]
 end
